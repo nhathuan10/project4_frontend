c65650bf4c834e47c20fd9df3371de6becbfc099
Huan <hphan298@gmail.com>
2023-02-12 19:11:05 +0700

    2-12-2023

---
+++ src/assets/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+type Props = {}
+
+export default function ({ }: Props) {
+    return (
+        // <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+        //     <div className='container-fluid'>
+        //         <span className='navbar-brand'>My Library</span>
+        //         <button
+        //             className='navbar-toggler'
+        //             type='button'
+        //             data-bs-toggle='collapse'
+        //             data-bs-target='#navbarNavDropdown'
+        //             arial-controls='navbarNavDropdown'
+        //             aria-expanded='false'
+        //             aria-label='Toggle Navigation'
+        //         >
+        //             <span className='navbar-toggler-icon'></span>
+        //         </button>
+        //         <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+        //             <ul className='navbar-nav'>
+        //                 <li className='nav-item'>
+        //                     <a className='nav-link' href='*'>Home</a>
+        //                 </li>
+        //                 <li className='nav-item'>
+        //                     <a className='nav-link' href='*'>Search Books</a>
+        //                 </li>
+        //             </ul>
+        //             <ul className='navbar-nav ms-auto'>
+        //                 <li className='nav-item'>
+        //                     <a type='button' className='btn btn-outline-light' href='*'>Sign in</a>
+        //                 </li>
+        //                 <li>
+        //                     <button className='btn b btn-outline-light'>Logout</button>
+        //                 </li>
+                        
+        //             </ul>
+        //         </div>
+        //     </div>
+        //     <p className='huan'>asd</p>
+        // </nav>
+        <div className='huan'>
+            <p className='nhat'>huan</p>
+        </div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import './App.css'
+import Navbar from './assets/components/Navbar';
+import './assets/scss/style.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <div>
-    HUan
-  </div>
+  <>
+    <Navbar />
+  </>
 );
 
 
