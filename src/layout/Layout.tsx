aa463f8b104321385f3b8395f3f255b35cd43000
Huan <hphan298@gmail.com>
2023-02-15 21:11:44 +0700

    2-15-2023

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Footer from '../assets/components/Footer'
-import Navbar from '../assets/components/Navbar'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 type Props = {}
 
