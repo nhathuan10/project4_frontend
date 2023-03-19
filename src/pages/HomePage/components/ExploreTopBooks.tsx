197a4a293da9cff116c1c3b775263b7341c9406c
Huan <hphan298@gmail.com>
2023-03-19 11:13:26 +0700

    3-19

--- src/pages/HomePage/components/ExploreTopBooks.tsx
+++ src/pages/HomePage/components/ExploreTopBooks.tsx
@@ -7,7 +7,7 @@ export default function ExploreTopBook({ }: Props) {
     return (
         <div className='p-5 mb-4 header d-flex justify-content-center align-items-center'>
             <div className='container-fluid text-white text-center'>
-                <h1 className='heading-primary'>Find your next adventure</h1>
+                <h1 className='heading-primary'>Best Library On Earth</h1>
             </div>
         </div>
     )
